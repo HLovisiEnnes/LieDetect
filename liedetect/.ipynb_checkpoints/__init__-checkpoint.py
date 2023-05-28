27b35eb960a82694f57f5b71a685a75e93e14460
Henrique Ennes <hlovisiennes@gmail.com>
2023-05-28 12:52:47 -0300

    05/28/2023

---
+++ liedetect/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+from .datasets import *
+from .matrix_manipulation import *
+from .lie_pca import *
+from .lie_optimization import *
+from .FastMatrixOptimization import *
+
+__version__ = '2023.05.29'
--- liedetect/__init__.py
+++ liedetect/__init__.py
@@ -2,5 +2,6 @@ from .datasets import *
 from .matrix_manipulation import *
 from .lie_pca import *
 from .lie_optimization import *
+from .FastMatrixOptimization import *
 
-__version__ = '2023.04.29'
+__version__ = '2023.05.29'
